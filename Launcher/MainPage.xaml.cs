6b0bed362c67100832440ff0d59a8d85160b430c
MahmoudElgml <32942446+MahmoudElgml@users.noreply.github.com>
2022-09-13 07:33:02 +0200

    enhance componenets

--- Launcher/MainPage.xaml.cs
+++ Launcher/MainPage.xaml.cs
@@ -1,9 +1,24 @@
-﻿using System.Diagnostics;
+﻿using Microsoft.Win32;
+using System.Diagnostics;
 
 namespace Launcher;
 
 public partial class MainPage : ContentPage
 {
+    public static string visualStodioExe => @"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe";
+    public static string dockerExe => @"C:\Program Files\Docker\Docker\Docker Desktop.exe";
+    public static string vsCodeExe => LocateExe("code");
+
+    public List<APGService> visualStuioServicesList = new List<APGService>
+        {
+            new APGService { serviceName = "APGFundamental", servicePath = "APG.FundamentalModulesAPIs\\APGFundamentals.sln",exePath=visualStodioExe},
+            new APGService { serviceName = "APGMembership", servicePath = "APG.MembershipModulesAPIs\\APGMembership.sln",exePath=visualStodioExe},
+            new APGService { serviceName = "APGPortalAPiGateway", servicePath = "APG.PortalAPIGateway\\APGPortalAPIGateway.sln", exePath = visualStodioExe},
+            new APGService { serviceName = "APGTransaction", servicePath = "APG.TransactionModulesAPIs\\APGTransaction.sln", exePath = visualStodioExe},
+            new APGService { serviceName = "APGExecution", servicePath = "APG.ExecutionModulesAPIs\\APGExecutions.sln", exePath = visualStodioExe},
+            new APGService { serviceName = "APGLog", servicePath = "APG.LogModulesAPIs\\APGLogs.sln", exePath = visualStodioExe},
+            new APGService { serviceName = "APGNotification", servicePath = "APG.NotificationModulesAPIs\\APGNotifications.sln", exePath = visualStodioExe}
+        };
     public MainPage()
     {
         InitializeComponent();
@@ -12,48 +27,48 @@ public partial class MainPage : ContentPage
 
     protected override void OnAppearing()
     {
-        var servicesList = new List<APGService>
-        {
-            new APGService { serviceName = "APGFundamental", servicePath = "APG.FundamentalModulesAPIs\\APGFundamentals.sln"},
-            new APGService { serviceName = "APGMembership", servicePath = "APG.MembershipModulesAPIs\\APGMembership.sln"},
-            new APGService { serviceName = "APGPortalAPiGateway", servicePath = "APG.PortalAPIGateway\\APGPortalAPIGateway.sln"},
-            new APGService { serviceName = "APGTransaction", servicePath = "APG.TransactionModulesAPIs\\APGTransaction.sln"},
-            new APGService { serviceName = "APGExecution", servicePath = "APG.ExecutionModulesAPIs\\APGExecutions.sln"},
-            new APGService { serviceName = "APGLog", servicePath = "APG.LogModulesAPIs\\APGLogs.sln"},
-            new APGService { serviceName = "APGNotification", servicePath = "APG.NotificationModulesAPIs\\APGNotifications.sln"}
-        };
-
-        collectionView.ItemsSource = servicesList;
+        collectionView.ItemsSource = visualStuioServicesList;
     }
+
     private void OnCounterClicked(object sender, EventArgs e)
     {
-        var visualStodioExe = @"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe";
         var apgServicesRoot = @"D:\git-repos\amwal-pay\";
-
-        var pathList= new List<string>();
-        var allCheckBoxex = collectionView.GetVisualTreeDescendants().Where(p => p.GetType() == typeof(CheckBox)).Where(p => (p as CheckBox).IsLoaded == true).ToList();
+        var allCheckBoxex = collectionView.GetVisualTreeDescendants().OfType<CheckBox>().Where(p => p.IsLoaded && p.IsChecked).ToList();
         foreach (CheckBox chbx in allCheckBoxex)
         {
-            if (chbx.IsChecked )
+            var service = chbx.BindingContext as APGService;
+            if (!string.IsNullOrWhiteSpace(service.exePath))
             {
-                pathList.Add(apgServicesRoot+(chbx.BindingContext as APGService).servicePath);
+                Process.Start(service.exePath, apgServicesRoot + service.servicePath);
             }
         }
-        foreach (var servicePath in pathList)
-        {
-            Process.Start(visualStodioExe, servicePath);
-        }
 
     }
 
-    void OnCheckBoxCheckedChanged(object sender, CheckedChangedEventArgs e)
+    private static string LocateExe(String filename)
     {
-       
+        String path = Environment.GetEnvironmentVariable("path");
+        String[] folders = path.Split(';');
+        foreach (String folder in folders)
+        {
+            if (File.Exists(folder + filename))
+            {
+                return folder + filename;
+            }
+            else if (File.Exists(folder + "\\" + filename))
+            {
+                return folder + "\\" + filename;
+            }
+        }
+        return String.Empty;
     }
+
 }
 
 public class APGService
 {
     public string serviceName { get; set; }
     public string servicePath { get; set; }
+    public string exePath { get; set; }
 }
+
