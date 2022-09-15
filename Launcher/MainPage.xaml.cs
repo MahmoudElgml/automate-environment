d478913c9eb800b120f3251bef7c29b72ee590ca
MahmoudElgml <32942446+MahmoudElgml@users.noreply.github.com>
2022-09-16 01:05:33 +0200

    final release version 1.0.0

--- Launcher/MainPage.xaml.cs
+++ Launcher/MainPage.xaml.cs
@@ -14,19 +14,19 @@ public partial class MainPage : ContentPage
 
     public List<APGService> visualStuioServicesList = new List<APGService>
         {
-            new APGService { serviceName = "APGPortalAPiGateway", servicePath = "APG.PortalAPIGateway\\APGPortalAPIGateway.sln", exePath = visualStodioExePath},//17
-            new APGService { serviceName = "APGTransaction", servicePath = "APG.TransactionModulesAPIs\\APGTransaction.sln", exePath = visualStodioExePath},
+            new APGService { serviceName = "APGPortalAPiGateway", servicePath = "\\APG.PortalAPIGateway\\APGPortalAPIGateway.sln", exePath = visualStodioExePath},//17
+            new APGService { serviceName = "APGTransaction", servicePath = "\\APG.TransactionModulesAPIs\\APGTransaction.sln", exePath = visualStodioExePath},
             new APGService { serviceName = "Docker", servicePath = null, exePath = dockerExePath},
-            new APGService { serviceName = "APGFundamental", servicePath = "APG.FundamentalModulesAPIs\\APGFundamentals.sln",exePath=visualStodioExePath},//20
-            new APGService { serviceName = "APGExecution", servicePath = "APG.ExecutionModulesAPIs\\APGExecutions.sln", exePath = visualStodioExePath},
-            new APGService { serviceName = "APGSmartBoxApiGateway", servicePath = "APG.SmartBoxAPIGateway\\APGSmartBoxAPIGateway.sln", exePath = visualStodioExePath},
-            new APGService { serviceName = "APGMembership", servicePath = "APG.MembershipModulesAPIs\\APGMembership.sln",exePath=visualStodioExePath},//23
-            new APGService { serviceName = "APGNotification", servicePath = "APG.NotificationModulesAPIs\\APGNotifications.sln", exePath = visualStodioExePath},
-            new APGService { serviceName = "APGeCommerceSmartBox", servicePath = "APG.eCommerceSmartBox", exePath = vsCodeExePath},
-            new APGService { serviceName = "APGPortal", servicePath = "APG.Portal", exePath = vsCodeExePath},//26
-            new APGService { serviceName = "APGLog", servicePath = "APG.LogModulesAPIs\\APGLogs.sln", exePath = visualStodioExePath},
-            new APGService { serviceName = "APGeCommerceSmartBox.Proxy", servicePath = "APG.eCommerceSmartBox.Proxy", exePath = vsCodeExePath},
-            new APGService { serviceName = "APGPortal.Proxy", servicePath = "APG.Portal.Proxy", exePath = vsCodeExePath},//29
+            new APGService { serviceName = "APGFundamental", servicePath = "\\APG.FundamentalModulesAPIs\\APGFundamentals.sln",exePath=visualStodioExePath},//20
+            new APGService { serviceName = "APGExecution", servicePath = "\\APG.ExecutionModulesAPIs\\APGExecutions.sln", exePath = visualStodioExePath},
+            new APGService { serviceName = "APGSmartBoxApiGateway", servicePath = "\\APG.SmartBoxAPIGateway\\APGSmartBoxAPIGateway.sln", exePath = visualStodioExePath},
+            new APGService { serviceName = "APGMembership", servicePath = "\\APG.MembershipModulesAPIs\\APGMembership.sln",exePath=visualStodioExePath},//23
+            new APGService { serviceName = "APGNotification", servicePath = "\\APG.NotificationModulesAPIs\\APGNotifications.sln", exePath = visualStodioExePath},
+            new APGService { serviceName = "APGeCommerceSmartBox", servicePath = "\\APG.eCommerceSmartBox", exePath = vsCodeExePath},
+            new APGService { serviceName = "APGPortal", servicePath = "\\APG.Portal", exePath = vsCodeExePath},//26
+            new APGService { serviceName = "APGLog", servicePath = "\\APG.LogModulesAPIs\\APGLogs.sln", exePath = visualStodioExePath},
+            new APGService { serviceName = "APGeCommerceSmartBox.Proxy", servicePath = "\\APG.eCommerceSmartBox.Proxy", exePath = vsCodeExePath},
+            new APGService { serviceName = "APGPortal.Proxy", servicePath = "\\APG.Portal.Proxy", exePath = vsCodeExePath},//29
         };
 
     public MainPage(IFolderPicker folderPicker)
@@ -47,7 +47,7 @@ public partial class MainPage : ContentPage
 
     private void OnStartClicked(object sender, EventArgs e)
     {
-        var apgServicesRoot = @"D:\git-repos\amwal-pay\";
+        var apgServicesRoot = rootProjectsPath;
         var allCheckBoxex = collectionView.GetVisualTreeDescendants().OfType<CheckBox>().Where(p => p.IsLoaded && p.IsChecked).ToList();
         foreach (CheckBox chbx in allCheckBoxex)
         {
@@ -88,7 +88,7 @@ public partial class MainPage : ContentPage
         {
             using (StreamReader sr = new StreamReader(@"D:\defaultAPGDirectory.txt"))
             {
-                rootProjectsPath = sr.ReadToEnd();
+                rootProjectsPath = sr.ReadLine();
                 FolderLabel.Text = rootProjectsPath;
             }
         }
