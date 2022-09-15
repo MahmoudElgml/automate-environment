af9a8c66b386f6d6209227bab6f864c439bed954
MahmoudElgml <32942446+MahmoudElgml@users.noreply.github.com>
2022-09-16 00:32:07 +0200

    enhance design

--- Launcher/MainPage.xaml.cs
+++ Launcher/MainPage.xaml.cs
@@ -14,19 +14,19 @@ public partial class MainPage : ContentPage
 
     public List<APGService> visualStuioServicesList = new List<APGService>
         {
-            new APGService { serviceName = "Docker", servicePath = null, exePath = dockerExePath},
-            new APGService { serviceName = "APGFundamental", servicePath = "APG.FundamentalModulesAPIs\\APGFundamentals.sln",exePath=visualStodioExePath},
-            new APGService { serviceName = "APGMembership", servicePath = "APG.MembershipModulesAPIs\\APGMembership.sln",exePath=visualStodioExePath},
-            new APGService { serviceName = "APGPortalAPiGateway", servicePath = "APG.PortalAPIGateway\\APGPortalAPIGateway.sln", exePath = visualStodioExePath},
+            new APGService { serviceName = "APGPortalAPiGateway", servicePath = "APG.PortalAPIGateway\\APGPortalAPIGateway.sln", exePath = visualStodioExePath},//17
             new APGService { serviceName = "APGTransaction", servicePath = "APG.TransactionModulesAPIs\\APGTransaction.sln", exePath = visualStodioExePath},
+            new APGService { serviceName = "Docker", servicePath = null, exePath = dockerExePath},
+            new APGService { serviceName = "APGFundamental", servicePath = "APG.FundamentalModulesAPIs\\APGFundamentals.sln",exePath=visualStodioExePath},//20
             new APGService { serviceName = "APGExecution", servicePath = "APG.ExecutionModulesAPIs\\APGExecutions.sln", exePath = visualStodioExePath},
             new APGService { serviceName = "APGSmartBoxApiGateway", servicePath = "APG.SmartBoxAPIGateway\\APGSmartBoxAPIGateway.sln", exePath = visualStodioExePath},
+            new APGService { serviceName = "APGMembership", servicePath = "APG.MembershipModulesAPIs\\APGMembership.sln",exePath=visualStodioExePath},//23
             new APGService { serviceName = "APGNotification", servicePath = "APG.NotificationModulesAPIs\\APGNotifications.sln", exePath = visualStodioExePath},
-            new APGService { serviceName = "APGLog", servicePath = "APG.LogModulesAPIs\\APGLogs.sln", exePath = visualStodioExePath},
-            new APGService { serviceName = "APGPortal", servicePath = "APG.Portal", exePath = vsCodeExePath},
-            new APGService { serviceName = "APGPortal.Proxy", servicePath = "APG.Portal.Proxy", exePath = vsCodeExePath},
             new APGService { serviceName = "APGeCommerceSmartBox", servicePath = "APG.eCommerceSmartBox", exePath = vsCodeExePath},
-            new APGService { serviceName = "APGeCommerceSmartBox.Proxy", servicePath = "APG.eCommerceSmartBox.Proxy", exePath = vsCodeExePath}
+            new APGService { serviceName = "APGPortal", servicePath = "APG.Portal", exePath = vsCodeExePath},//26
+            new APGService { serviceName = "APGLog", servicePath = "APG.LogModulesAPIs\\APGLogs.sln", exePath = visualStodioExePath},
+            new APGService { serviceName = "APGeCommerceSmartBox.Proxy", servicePath = "APG.eCommerceSmartBox.Proxy", exePath = vsCodeExePath},
+            new APGService { serviceName = "APGPortal.Proxy", servicePath = "APG.Portal.Proxy", exePath = vsCodeExePath},//29
         };
 
     public MainPage(IFolderPicker folderPicker)
